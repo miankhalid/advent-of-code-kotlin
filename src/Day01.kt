3b8fb2a200e0928c717edd67b2b019a9a004cc75
Mian Khalid <mkhalid@edx.org>
2023-12-05 14:06:46 +0500

    Add Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val digits = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+    val numeralsMap = mapOf(
+        "zero" to "z0o", "one" to "o1e","two" to "t2o","three" to "t3e","four" to "f4r",
+        "five" to "f5e","six" to "s6x","seven" to "s7n","eight" to "e8t","nine" to "n9e")
+
+    val input = readInput("files/input-day-1")
+    var sum = 0
+    for (s in input) {
+        var replaceableString = s
+        for (pair in numeralsMap) {
+            replaceableString = replaceableString.replace(pair.key, pair.value)
+        }
+        val char1 = replaceableString.split("").find {it in digits}
+        val char2 = replaceableString.split("").findLast {it in digits}
+
+        val reading = char1 + char2
+        sum += reading.toInt()
+        println("$replaceableString --> $reading --> $sum")
+
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
 }
